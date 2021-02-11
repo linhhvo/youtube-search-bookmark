2392af85f9ad476606ceb5dea4ecc59324772f06
Linh <lhvo2112@gmail.com>
2021-02-11 16:48:45 -0500

    Refactor video list component

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import classes from './Video.module.css';
+// import classes from './Video.module.css';
 
 export const Video = (props) => {
 
   return (
-    <div className={classes.container}>
-      {props.videoIds.map(videoId =>
-        <iframe key={videoId} width="356" height="200" src={"https://www.youtube.com/embed/" + videoId} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      )}
-    </div >
+    <iframe width="356" height="200" src={"https://www.youtube.com/embed/" + props.videoId} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
   );
 };
--- client/src/components/VideoList.js
+++ client/src/components/VideoList.js
@@ -1,15 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, Fragment} from 'react';
 import {Video} from './Video';
 import {GlobalContext} from '../context/GlobalState';
+import classes from './VideoList.module.css';
 
 export const VideoList = () => {
   const {displayedItem} = useContext(GlobalContext);
 
-  let videos = Object.keys(displayedItem).length !== 0 ? <Video key={displayedItem.keyword} videoIds={displayedItem.videoIds} /> : null;
+  let videos = Object.keys(displayedItem).length !== 0 ?
+    <div className={classes.container}>
+      {displayedItem.videoIds.map(videoId => <Video key={videoId} videoId={videoId} />)}
+    </div >
+    : null;
 
   return (
-    <div>
+    <Fragment>
       {videos}
-    </div>
+    </Fragment>
   );
 };
