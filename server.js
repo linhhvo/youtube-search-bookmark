2477221035e77cd0bd5955f55bd5b358fa9b0685
Linh <lhvo2112@gmail.com>
2021-04-10 15:20:24 -0400

    Added build script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const path = require('path');
 
 // Get global env variable
 dotenv.config({path: './config/config.env'});
@@ -16,6 +17,15 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/videos', videos);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || '5000';
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
