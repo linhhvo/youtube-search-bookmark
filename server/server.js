b2f4d380f916b868584707632fb5b270961dcd51
Linh Vo <lhvo2112@gmail.com>
2024-12-28 21:13:15 +0200

    deploy to fly

--- server/server.js
+++ server/server.js
@@ -28,6 +28,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || '3000';
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
